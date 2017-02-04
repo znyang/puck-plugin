a0c843f87a4f5e289f4828d22fe924b712687f57
zenyang <xggcl@sina.com>
2017-02-04 13:12:32 +0800

    fixed core version
    Change-Id: Ifc82bfdd4f8c3e33e24f90139a054386ff4a235b

--- plugin/src/main/templates/Version.java
+++ plugin/src/main/templates/Version.java
@@ -1,6 +1,6 @@
 package com.zen.android.puck;
 
 public class Version {
-    public static final String GROUP = "com.github.znyang.puck";
+    public static final String GROUP = "com.github.znyang";
     public static final String VERSION = "@version@";
 }
